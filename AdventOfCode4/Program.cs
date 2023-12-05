4bd95cc909988a0303d7465fed0c1a4b198c8f10
Mattéo Tatibouët <matteo@enki-digital.fr>
2023-12-05 09:44:04 +0100

    Adds day 4 part 2

--- AdventOfCode4/Program.cs
+++ AdventOfCode4/Program.cs
@@ -1,9 +1,18 @@
 ﻿string[] inputLines = File.ReadAllLines("input.txt");
 
-int totalScore = 0;
+// Dictionary ou les valeurs sont le nombre de cartes, 1 de base
+Dictionary<string, int> inputLinesDict = inputLines.ToDictionary(key => key, value => 1);
 
-foreach (string line in inputLines)
+// ToArray() pour pouvoir accéder avec index
+(string Card, int Quantity)[] cards = inputLinesDict.Select(element => (Card: element.Key, Quantity: element.Value)).ToArray();
+
+int part1TotalScore = 0;
+int part2TotalScore = 0;
+
+// Part 1
+for (int i = 0; i < cards.Length; i++)
 {
+    string line = cards[i].Card;
     string card = line.Split(':')[1];
     string leftSide = card.Split('|')[0];
     string rightSide = card.Split('|')[1];
@@ -27,7 +36,49 @@ foreach (string line in inputLines)
         }
     }
 
-    totalScore += cardScore;
+    part1TotalScore += cardScore;
 }
 
-Console.WriteLine($"Total score : {totalScore}");
+// Part 2
+for (int i = 0; i < cards.Length; i++)
+{
+	string line = cards[i].Card;
+	string card = line.Split(':')[1];
+	string leftSide = card.Split('|')[0];
+	string rightSide = card.Split('|')[1];
+
+	string[] winningNumbersString = leftSide.Split(' ');
+	string[] playedNumbersString = rightSide.Split(' ');
+
+	winningNumbersString = winningNumbersString.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+	playedNumbersString = playedNumbersString.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
+
+	int[] winningNumbers = Array.ConvertAll(winningNumbersString, Int32.Parse);
+	int[] playedNumbers = Array.ConvertAll(playedNumbersString, Int32.Parse);
+
+	int cardsWon = 0;
+
+	foreach (int number in playedNumbers)
+	{
+		if (winningNumbers.Contains(number))
+		{
+			cardsWon += 1;
+		}
+	}
+
+	for (int j = 0; j < cards[i].Quantity; j++)
+	{
+		for (int k = 1; k <= cardsWon; k++)
+		{
+			cards[i + k].Quantity += 1;
+		}
+	}
+}
+
+foreach (var card in cards)
+{
+    part2TotalScore += card.Quantity;
+}
+
+Console.WriteLine($"Part 1 total score : {part1TotalScore}");
+Console.WriteLine($"Part 2 total score : {part2TotalScore}");
