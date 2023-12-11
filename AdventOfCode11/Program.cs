65777dbf722f3f287c1ea917503fc03c88cdf8c7
matos <18725330+tosmatos@users.noreply.github.com>
2023-12-11 16:43:25 +0100

    Progress on Day 11 part 1, need to calculate distance for pairs

---
+++ AdventOfCode11/Program.cs
@@ -0,0 +1,124 @@
+﻿string[] input = File.ReadAllLines("input.txt");
+
+char[,] grid = new char[input.Length, input[0].Length];
+
+// Transform string[] to char[,]
+for (int i = 0; i < input.Length; i++)
+{
+	string currentString = input[i];
+	for (int j = 0; j < input[i].Length; j++)
+	{
+		grid[i, j] = currentString[j];
+	}
+}
+
+int rowCount = grid.GetLength(0);
+int columnCount = grid.GetLength(1);
+
+List<int> rowIndexes = [];
+
+// Get rows to expand
+for (int i = 0; i < rowCount; i++)
+{
+	if (!input[i].Contains('#'))
+		rowIndexes.Add(i);
+}
+
+List<int> columnIndexes = [];
+
+//Get columns to expand
+for (int i = 0; i < columnCount; i++)
+{
+	List<char> column = [];
+	for (int j = 0; j < rowCount; j++)
+	{
+		column.Add(grid[j, i]);
+	}
+	if (!column.Contains('#'))
+		columnIndexes.Add(i);
+}
+
+char[,] newGrid;
+// Insert rows
+foreach (int rowIndex in rowIndexes)
+{
+	newGrid = InsertRow(grid, rowIndex);
+	grid = new char[newGrid.GetLength(0), newGrid.GetLength(1)];
+	Array.Copy(newGrid, grid, newGrid.Length);
+}
+
+foreach (int columnIndex in columnIndexes)
+{
+	newGrid = InsertColumn(grid, columnIndex);
+	grid = new char[newGrid.GetLength(0), newGrid.GetLength(1)];
+	Array.Copy(newGrid, grid, newGrid.Length);
+}
+
+List<(int Row, int Column)> galaxies = [];
+
+for (int i = 0; i < grid.GetLength(0); i++)
+{
+	for (int j = 0; j < grid.GetLength(1); j++)
+	{
+		if (grid[i, j] == '#')
+			galaxies.Add((i, j));
+	}
+}
+
+Console.WriteLine();
+
+char[,] InsertRow(char[,] originalGrid, int rowIndex)
+{
+	int rows = originalGrid.GetLength(0);
+	int cols = originalGrid.GetLength(1);
+
+	char[,] newGrid = new char[rows + 1, cols];
+
+	for (int i = 0; i < rowIndex; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			newGrid[i, j] = originalGrid[i, j];
+		}
+	}
+
+	for (int j = 0; j < cols; j++)
+	{
+		newGrid[rowIndex, j] = '.'; // You can set the default value as needed
+	}
+
+	for (int i = rowIndex + 1; i < rows + 1; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			newGrid[i, j] = originalGrid[i - 1, j];
+		}
+	}
+
+	return newGrid;
+}
+
+char[,] InsertColumn(char[,] originalGrid, int colIndex)
+{
+	int rows = originalGrid.GetLength(0);
+	int cols = originalGrid.GetLength(1);
+
+	char[,] newGrid = new char[rows, cols + 1];
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < colIndex; j++)
+		{
+			newGrid[i, j] = originalGrid[i, j];
+		}
+
+		newGrid[i, colIndex] = '.'; // You can set the default value as needed
+
+		for (int j = colIndex + 1; j < cols + 1; j++)
+		{
+			newGrid[i, j] = originalGrid[i, j - 1];
+		}
+	}
+
+	return newGrid;
+}
