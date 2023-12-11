66cfac81f9d5ed13aeba49bd7b94ede78d6ee5f1
matos <18725330+tosmatos@users.noreply.github.com>
2023-12-11 11:41:36 +0100

    Adds solution part 2

--- AdventOfCode9/Program.cs
+++ AdventOfCode9/Program.cs
@@ -1,10 +1,11 @@
 ﻿string[] input = File.ReadAllLines("input.txt");
 
-int totalPart1 = 0;
+int total = 0;
 
 foreach (string line in input)
 {
-	int[] searchedLine = Array.ConvertAll(line.Split(' '), Convert.ToInt32);
+	// Reverse searchedLine to get answer for part 2
+	int[] searchedLine = Array.ConvertAll(line.Split(' '), Convert.ToInt32); //.Reverse().ToArray();
 	List<int> newLine = [];
 	int extrapolatedValue = searchedLine.Last();
 	bool allZeroes = false;
@@ -13,12 +14,12 @@ foreach (string line in input)
 	{
 		for (int i = 0; i < searchedLine.Length - 1; i++)
 		{
-			//int max = Math.Max(searchedLine[i], searchedLine[i + 1]);
-			//int min = Math.Min(searchedLine[i], searchedLine[i + 1]);
 			newLine.Add(searchedLine[i + 1] - searchedLine[i]);
 		}
-		extrapolatedValue += newLine.LastOrDefault();
+
+		extrapolatedValue += newLine.Last();
 		int zeroCount = newLine.Where(number => number == 0).Count();
+
 		if (newLine.Count() == zeroCount)
 			allZeroes = true;
 		else
@@ -28,8 +29,6 @@ foreach (string line in input)
 			newLine = [];
 		}
 	}
-
-	totalPart1 += extrapolatedValue;
+	total += extrapolatedValue;
 }
-
-Console.WriteLine($"Sum of all extrapolated values (part 1) : {totalPart1}");
+Console.WriteLine($"Sum of all extrapolated values (part 1) : {total}");
