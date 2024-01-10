124b8b1c9900026e022ceca7b54b82c9fea5f6a1
matos <18725330+tosmatos@users.noreply.github.com>
2024-01-10 16:35:40 +0100

    Could bruteforce day 14 part 2 in around 3 days

--- AdventOfCode14/Program.cs
+++ AdventOfCode14/Program.cs
@@ -20,7 +20,21 @@ TiltGridNorth();
 DisplayGrid();
 ComputeLoad();
 
-Console.WriteLine("Total load : " + totalLoad);
+Console.WriteLine("Total load with only north tilt : " + totalLoad);
+totalLoad = 0;
+
+for (int i = 0; i < 1000000000; i++)
+{
+	TiltGridNorth();
+	TiltGridWest();
+	TiltGridSouth();
+	TiltGridEast();
+}
+
+DisplayGrid();
+ComputeLoad();
+
+Console.WriteLine("Total load after 1000000000 cycles : " + totalLoad);
 
 //Console.WriteLine(grid[grid.GetLength(0) - 1, grid.GetLength(1) - 1]);
 
@@ -58,6 +72,81 @@ void TiltGridNorth()
 					{
 						grid[row, column] = 'O';
 						grid[rowUnder, column] = '.';
+						movedRocks = true;
+					}
+				}
+			}
+		}
+	}
+}
+
+void TiltGridSouth()
+{
+	bool movedRocks = true;
+	while (movedRocks)
+	{
+		movedRocks = false;
+		for (int row = grid.GetLength(0) - 1; row >= 0; row--)
+		{
+			int rowAbove = row - 1;
+			for (int column = 0; column < grid.GetLength(1); column++)
+			{
+				if (grid[row, column] == '.')
+				{
+					if (rowAbove >= 0 && grid[rowAbove, column] == 'O')
+					{
+						grid[row, column] = 'O';
+						grid[rowAbove, column] = '.';
+						movedRocks = true;
+					}
+				}
+			}
+		}
+	}
+}
+
+void TiltGridEast()
+{
+	bool movedRocks = true;
+	while (movedRocks)
+	{
+		movedRocks = false;
+		for (int row = 0; row < grid.GetLength(0); row++)
+		{
+			for (int column = 0; column < grid.GetLength(1); column++)
+			{
+				int columnBefore = column - 1;
+				if (grid[row, column] == '.')
+				{
+					if (columnBefore >= 0 && grid[row, columnBefore] == 'O')
+					{
+						grid[row, column] = 'O';
+						grid[row, columnBefore] = '.';
+						movedRocks = true;
+					}
+				}
+			}
+		}
+	}
+}
+
+void TiltGridWest()
+{
+	bool movedRocks = true;
+	while (movedRocks)
+	{
+		movedRocks = false;
+		for (int row = 0; row < grid.GetLength(0); row++)
+		{
+			for (int column = grid.GetLength(1) - 1; column >= 0; column--)
+			{
+				int columnBefore = column + 1;
+				if (grid[row, column] == '.')
+				{
+					if (columnBefore < grid.GetLength(1) && grid[row, columnBefore] == 'O')
+					{
+						grid[row, column] = 'O';
+						grid[row, columnBefore] = '.';
 						movedRocks = true;
 					}
 				}
