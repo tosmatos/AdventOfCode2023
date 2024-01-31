a8d8d048f602782d90474487fb4c766b15473783
matos <18725330+tosmatos@users.noreply.github.com>
2024-01-31 09:21:04 +0100

    Solution for day 15 part 1

---
+++ AdventOfCode15/Program.cs
@@ -0,0 +1,29 @@
+﻿string input = File.ReadAllText("input.txt");
+
+input = input.Replace("\n", String.Empty);
+
+int currentValue = 0;
+int totalValues = 0;
+
+Dictionary<char, int> charPlusAscii = new();
+
+foreach (char c in input)
+{
+	if (c == ',')
+	{
+		totalValues += currentValue;
+		currentValue = 0;
+		continue;
+	}
+
+	currentValue += (int)c;
+	currentValue *= 17;
+	currentValue %= 256;
+
+	if (!charPlusAscii.ContainsKey(c))
+		charPlusAscii.Add(c, c);
+}
+
+totalValues += currentValue; // Account last step not having a comma after it
+
+Console.WriteLine($"Sum of HASH's : {totalValues}");
